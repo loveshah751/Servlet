4ac269544e0079b7329da567d5bfae3df8c3ff0b
LOVEPREET <lovepreet.singh@adp.com>
2020-10-11 12:32:00 -0700

    first commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        var writer = resp.getWriter();
+
+        writer.println("Hello World from "+this.getClass().getSimpleName() +" class at around "+ LocalDateTime.now());
+    }
+
+}
